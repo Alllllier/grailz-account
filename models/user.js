3e00a2227865858a3b96dd8378f96d412b1307fe
Ally <allier0416@gmail.com>
2018-12-09 22:54:56 -0800

    init

---
+++ models/user.js
@@ -0,0 +1,20 @@
+var mongoose = require("mongoose");
+var userSchema = mongoose.Schema({
+    email: String,
+    password: String,
+    viewHistory: [{
+        product_id: String,
+        title: String,
+        image: String,
+    }],
+    watchlist: [{
+        product_id: String,
+        date: String,
+        title: String,
+        image: String,
+        release: String
+    }]
+});
+
+var User = mongoose.model("User", userSchema);
+module.exports = User;
---
+++ routes/index.js
@@ -0,0 +1,231 @@
+var express = require('express');
+var router = express.Router();
+var User = require('../models/user');
+
+router.get('/', function(req, res) {
+  res.send({
+      message: "API for grailz account"
+  });
+});
+
+router.post('/user', function (req, res) {
+    var email = req.body.email;
+    var password = req.body.password;
+    User.findOne({ email: email }, function (err, user) {
+        if (err) {
+            req.send({
+                error: err
+            })
+        }
+        if (user) {
+            res.send({
+                error: "Email Already exist"
+            })
+        }
+        var newUser = new User({
+            email: email,
+            password: password
+        });
+        newUser.save().then(res.send({
+            result: "success"
+        }))
+    })
+});
+
+router.get('/user', function (req, res) {
+    User.findOne({ email: req.query.email }, function (err, user) {
+        if (err) {
+            res.send({
+                error: err
+            })
+        }
+        if (user) {
+            res.send({
+                email: user.email,
+                history: user.viewHistory,
+                watchlist: user.watchlist
+            })
+        }
+    })
+});
+
+router.post('/watchlist/', function (req, res) {
+    var email = req.body.email;
+    User.findOne({ email: email }, function (err, user) {
+        if (err) {
+            req.send({
+                error: err
+            })
+        }
+        if (user) {
+            user.watchlist.push({
+                product_id: req.body.product_id,
+                date: req.body.date,
+                title: req.body.title,
+                image: req.body.image,
+                release: req.body.release
+            });
+            user.save().then(function () {
+                res.send({
+                    result: "success"
+                })
+            }, function (err) {
+                res.send({
+                    error: err
+                })
+            });
+        } else {
+            res.send({
+                error: "Email not found"
+            })
+        }
+    })
+});
+
+router.get('/watchlist', function (req, res) {
+    User.findOne({ email: req.query.email }, function (err, user) {
+        if (err) {
+            req.send({
+                error: err
+            })
+        }
+        if (user) {
+            res.send(user.watchlist)
+        } else {
+            res.send({
+                error: "Email not found"
+            })
+        }
+    })
+});
+
+router.delete('/watchlist', function (req, res) {
+    var email = req.query.email;
+    var id = req.query.id;
+    var found = false;
+    User.findOne({ email: email }, function (err, user) {
+        if (err) { req.send({ error: err }) }
+        if (user) {
+            for (var i = 0; i < user.watchlist.length; i++) {
+                if (user.watchlist[i].product_id === id) {
+                    found = true;
+                    user.watchlist.splice(i, 1);
+                    user.save().then(res.send({result: "success"}))
+                }
+            }
+            if (!found) {
+                res.send({ error: "ID not found" })
+            }
+        } else {
+            res.send({
+                error: "Email not found"
+            })
+        }
+    })
+});
+
+router.post('/history', function (req, res) {
+    var email = req.body.email;
+    User.findOne({ email: email }, function (err, user) {
+        if (err) {
+            req.send({
+                error: err
+            })
+        }
+        if (user) {
+            user.viewHistory.push({
+                product_id: req.body.product_id,
+                title: req.body.title,
+                image: req.body.image
+            });
+            user.save().then(function () {
+                res.send({
+                    result: "success"
+                })
+            }, function (err) {
+                res.send({
+                    error: err
+                })
+            });
+        } else {
+            res.send({
+                error: "Email not found"
+            })
+        }
+    })
+});
+
+router.get('/history/', function (req, res) {
+    User.findOne({ email: req.query.email }, function (err, user) {
+        if (err) {
+            req.send({
+                error: err
+            })
+        }
+        if (user) {
+            res.send(user.viewHistory)
+        } else {
+            res.send({
+                error: "Email not found"
+            })
+        }
+    })
+});
+
+router.delete('/history', function (req, res) {
+    var email = req.query.email;
+    var id = req.query.id;
+    var found = false;
+    User.findOne({ email: email }, function (err, user) {
+        if (err) { req.send({ error: err }) }
+        if (user) {
+            for (var i = 0; i < user.viewHistory.length; i++) {
+                if (user.viewHistory[i].product_id === id) {
+                    found = true;
+                    user.viewHistory.splice(i, 1);
+                    user.save().then(res.send({result: "success"}))
+                }
+            }
+            if (!found) {
+                res.send({ error: "ID not found" })
+            }
+        } else {
+            res.send({
+                error: "Email not found"
+            })
+        }
+    })
+});
+
+
+
+router.post('/login', function (req, res) {
+  var email = req.body.email;
+  var password = req.body.password;
+  User.findOne({ email: email }, function (err, user) {
+      if (err) {
+          req.send({
+              error: err
+          })
+      }
+      if (user) {
+          if (user.password === password) {
+              return res.send({
+                  result: "success",
+                  email: user.email
+              })
+          } else {
+              return res.send({
+                  result: "failed",
+                  error: "Password incorrect!"
+              })
+          }
+      } else {
+          res.send({
+              error: "Email not found"
+          })
+      }
+  })
+});
+
+module.exports = router;
